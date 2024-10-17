af44dc9d1dfdcddc7a83597d347639a3c7adf15f
Thanh Huyen <thanhhuyenbb4683@gmail.com>
2024-10-17 22:42:41 +0700

    Create Usermethod.java

---
+++ Usermethod.java
@@ -0,0 +1,21 @@
+
+public class Usermethod {
+
+    // Method tạo người dùng
+    public void createUser() {
+        System.out.println("User created with ID: " + userId);
+    }
+
+    // Method reset mật khẩu
+    public void resetPassword(String newPassword) {
+        this.password = newPassword;
+        System.out.println("Password has been reset.");
+    }
+
+    // Hiển thị thông tin người dùng
+    public void displayUserInfo() {
+        System.out.println("User ID: " + userId);
+        System.out.println("Email: " + email);
+        System.out.println("User Type: " + userType);
+    }
+}
